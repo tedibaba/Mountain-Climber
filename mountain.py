c1f3b765e6eee00876454a06fc835a58585dbce2
tedibaba <randilhetti@gmail.com>
2023-09-12 12:46:30 +1000

    Passing test cases

--- mountain.py
+++ mountain.py
@@ -7,3 +7,24 @@ class Mountain:
     name: str
     difficulty_level: int
     length: int
+
+    def __lt__(self, mountain):
+        if self.difficulty_level < mountain.difficulty_level:
+            return True
+        elif self.difficulty_level > mountain.difficulty_level:
+            return False
+        elif self.name < mountain.name:
+            return True
+        else:
+            return False
+        
+    def __gt__(self, mountain):
+        if self.difficulty_level > mountain.difficulty_level:
+            return True
+        elif self.difficulty_level < mountain.difficulty_level:
+            return False
+        elif self.name > mountain.name:
+            return True
+        else:
+            return False
+
--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 
 from mountain import Mountain
+from algorithms.binary_search import binary_search
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.mountain_ranks = []
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
-
+        pos = binary_search(self.mountain_ranks, mountain)
+        if self.mountain_ranks[pos] == mountain:
+            return pos
+        else:
+            raise KeyError(mountain)
+         
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+        for mountain in mountains:
+            pos = binary_search(self.mountain_ranks, mountain)
+            self.mountain_ranks.insert(pos, mountain)
+
