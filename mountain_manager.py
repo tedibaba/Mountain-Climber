291d205c40d7e20badae9ce5e184addd1e9a93b7
tedibaba <randilhetti@gmail.com>
2023-09-13 20:42:18 +1000

    Passing test cases

--- mountain_manager.py
+++ mountain_manager.py
@@ -1,22 +1,37 @@
 from __future__ import annotations
 from mountain import Mountain
+from double_key_table import DoubleKeyTable
 
 class MountainManager:
 
     def __init__(self) -> None:
-        pass
+        self.mountains = DoubleKeyTable()
 
     def add_mountain(self, mountain: Mountain) -> None:
-        raise NotImplementedError()
+        self.mountains[str(mountain.difficulty_level), mountain.name] = mountain
+
 
     def remove_mountain(self, mountain: Mountain) -> None:
-        raise NotImplementedError()
+        try:
+            del self.mountains[str(mountain.difficulty_level), mountain.name]
+        except KeyError:
+            raise KeyError(mountain)
 
     def edit_mountain(self, old: Mountain, new: Mountain) -> None:
-        raise NotImplementedError()
+        del self.mountains[old.difficulty_level, old.name]
+        self.mountains[new.difficulty_level, new.name] = new
 
     def mountains_with_difficulty(self, diff: int) -> list[Mountain]:
-        raise NotImplementedError()
+        try:
+            return self.mountains.values(str(diff))
+        except KeyError:
+            return []
 
     def group_by_difficulty(self) -> list[list[Mountain]]:
-        raise NotImplementedError()
+        mountain_groups = []
+        difficulties = self.mountains.keys()
+        for difficulty in difficulties:
+            diff_mountains = self.mountains.values(difficulty)
+            mountain_groups.append(diff_mountains)
+        return mountain_groups
+        
