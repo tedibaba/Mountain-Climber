f17ecb1a455d21f8c070c210863237ee7f777580
tedibaba <randilhetti@gmail.com>
2023-09-18 08:54:38 +1000

    Hopefully working

--- trail.py
+++ trail.py
@@ -132,49 +132,40 @@ class Trail:
         pass
 
     def difficulty_difference_paths(self, max_difference: int) -> list[list[Mountain]]: # Input to this should not exceed k > 50, at most 5 branches.        
-        paths = []
-        min_max_path = [float("inf"), 0] #Kept in order as min, max
-        #Need to consider a trail series being at the start
-        # starting_path = self.traverse_trail_series(min_max_path, max_difference)
+        res = []
+        
+        paths = self.collect_all_paths()
+        for path in paths:
+            min_max_path = [float("inf"), 0] #Kept in order as min, max
+            for mountain in path:
+                min_max_path[0] = min(min_max_path[0], mountain.difficulty_level)
+                min_max_path[1] = max(min_max_path[1], mountain.difficulty_level)
+            if min_max_path[1] - min_max_path[0] <= max_difference:
+                res.append(path)
+        return res
 
-        return self.aux_difficulty_difference_paths([float("inf"), 0], [], paths, max_difference,0)
 
-
-    def aux_difficulty_difference_paths(self, min_max_path, curr_path, paths, max_difference, top_or_bot): #curr_path is the accumulator
-
+    def collect_all_paths(self):
         if isinstance(self.store, TrailSplit):
-            self.store.following.aux_difficulty_difference_paths(min_max_path, curr_path, paths, max_difference,0)
-
-            self.store.top.aux_difficulty_difference_paths(min_max_path + [], curr_path + [], paths, max_difference, 1)
-
-            self.store.bottom.aux_difficulty_difference_paths(min_max_path + [], curr_path + [], paths, max_difference, 1)
-
+            res = []
+            top, bottom, following = self.store.top.collect_all_paths(), self.store.bottom.collect_all_paths(), self.store.following.collect_all_paths()
+            top.extend(bottom)
+            for path in following:
+                for branch in top:
+                    res.append(branch + path)
+            return res
 
         elif isinstance(self.store, TrailSeries):
-            curr_path +=  self.traverse_trail_series(min_max_path) 
-            self.store.following.aux_difficulty_difference_paths(min_max_path, curr_path, paths, max_difference, top_or_bot)
-        
-        elif self == Trail(None) and top_or_bot:
-            if min_max_path[1] - min_max_path[0] <= max_difference:
-                paths.append(curr_path[::-1])
-    
-        return paths
+            res = []
+            following = self.store.following.collect_all_paths()
+            for path in following:
+                res.append([self.store.mountain] + path)
 
+            return res
 
-    def traverse_trail_series(self, min_max_path):
-        path_series = []
-        curr = self
-        if isinstance(curr.store, TrailSeries):
-        #These mountains must be traversed
-            while not isinstance(curr.store, TrailSplit):
-                    path_series.append(curr.store.mountain)
-                    min_max_path[0] = min(min_max_path[0], curr.store.mountain.difficulty_level)
-                    min_max_path[1] = max(min_max_path[1], curr.store.mountain.difficulty_level)
-                    if curr.store.following != Trail(None):
-                        curr = curr.store.following
-                    else:
-                        return path_series
-        return path_series
+        elif self == Trail(None):
+            return [[]]
+ 
 
 
 
@@ -185,10 +176,33 @@ class Trail:
 
 
 
+top_top = Mountain("top-top", 6, 3)
+top_bot = Mountain("top-bot", 3, 5)
+top_mid = Mountain("top-mid", 7, 2)
+bot_one = Mountain("bot-one", 2, 5)
+bot_two = Mountain("bot-two", 0, 0)
+final   = Mountain("final", 4, 4)
+trail = Trail(TrailSplit(
+Trail(TrailSplit(
+    Trail(TrailSeries(top_top, Trail(None))),
+    Trail(TrailSeries(top_bot, Trail(None))),
+    Trail(TrailSeries(top_mid, Trail(None))),
+)),
+Trail(TrailSeries(bot_one, Trail(TrailSplit(
+    Trail(TrailSeries(bot_two, Trail(None))),
+    Trail(TrailSplit(Trail(None), Trail(None), Trail(None))),
+    Trail(None),
+)))),
+Trail(TrailSeries(final, Trail(None)))
+))
 
+# trail = Trail(TrailSeries(bot_one, Trail(TrailSplit(
+#     Trail(TrailSeries(bot_two, Trail(TrailSeries(top_top, Trail(None))))),
+#     Trail(TrailSplit(Trail(None), Trail(None), Trail(None))),
+#     Trail(None)))))
+# trail =  Trail(TrailSeries(top_top, Trail(TrailSeries(top_bot, Trail(TrailSeries(top_mid, Trail(None)))))))
 
-
-
+print(trail.difficulty_difference_paths(3))
 
 
 
@@ -273,3 +287,81 @@ class Trail:
     #         return curr_path
     #     else:
     #         return curr_path1
+
+
+
+    # def traverse_trail_series(self, min_max_path):
+    #     path_series = []
+    #     curr = self
+    #     if isinstance(curr.store, TrailSeries):
+    #     #These mountains must be traversed
+    #         while not isinstance(curr.store, TrailSplit):
+    #                 path_series.append(curr.store.mountain)
+    #                 min_max_path[0] = min(min_max_path[0], curr.store.mountain.difficulty_level)
+    #                 min_max_path[1] = max(min_max_path[1], curr.store.mountain.difficulty_level)
+    #                 if curr.store.following != Trail(None):
+    #                     curr = curr.store.following
+    #                 else:
+    #                     return path_series
+    #     return path_series
+
+
+
+
+
+
+
+
+
+    # def aux_difficulty_difference_paths(self, min_max_path, curr_path, paths, max_difference, top_or_bot): #curr_path is the accumulator
+
+    #     if isinstance(self.store, TrailSplit):
+
+    #         self.store.top.aux_difficulty_difference_paths(min_max_path + [], curr_path + [], paths + [], max_difference, 1)
+
+    #         self.store.bottom.aux_difficulty_difference_paths(min_max_path + [], curr_path + [], paths + [], max_difference, 1)
+
+    #         self.store.following.aux_difficulty_difference_paths(min_max_path, curr_path, paths, max_difference,0)
+
+
+
+    #     elif isinstance(self.store, TrailSeries):
+    #         curr_path.append(self.store.mountain)
+    #         min_max_path[0] = min(min_max_path[0], self.store.mountain.difficulty_level)
+    #         min_max_path[1] = max(min_max_path[1], self.store.mountain.difficulty_level)
+    #         self.store.following.aux_difficulty_difference_paths(min_max_path, curr_path, paths, max_difference, top_or_bot)
+        
+    #     elif self == Trail(None) :
+    #         if min_max_path[1] - min_max_path[0] <= max_difference:
+    #             paths.append(curr_path)
+    
+    #     return paths
+
+
+
+        # if isinstance(self.store, TrailSplit):
+        #     paths = []
+        #     for arr in [self.store.top.collect_all_paths([])] + [self.store.bottom.collect_all_paths([])]:
+        #         if arr == [] :
+        #             paths.append([])
+        #         else:
+        #             paths.extend([elem if isinstance(elem, list) else [elem] for elem in arr])
+                   
+        #     following_path = self.store.following.collect_all_paths([], 0)
+        #     for path in paths:
+        #         path += following_path
+        #     return paths
+
+
+
+        # elif isinstance(self.store, TrailSeries):
+        #     paths.append(self.store.mountain)
+        #     following = self.store.following.collect_all_paths([], bam)
+        #     paths.extend([elem if isinstance(elem, list) else [elem] for elem in following])
+        #     return paths
+            
+
+        # elif self == Trail(None):  
+        #     # if len(paths) == 1 and bam:
+        #     #     return [paths]
+        #     return paths
